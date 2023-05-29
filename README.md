dedb82134b0d7b41cdfd5543da8f311400169364
htmltiger <1429451+htmltiger@users.noreply.github.com>
2023-05-29 21:18:46 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,11 @@
+# Simple 1-Wire Arduino library for the ESP32
+Arduino Library for ESP32 DS18B20 Maxim Integrated "1-Wire" protocol.
+
+The original implementation of OneWire library used CPU delays to construct the 1-Wire read/write timeslots however this proved to be too unreliable.
+It is based on the ESP's RMT peripheral. Whilst originally designed as an IR tranceiver it provides a good general purpose hardware bitstream encoder/decoder.
+
+Using the ESP32's RMT peripheral, results in very accurate read/write timeslots and more reliable operation.
+
+The example is provided and tested using Arduino 2.0.6 based on ESP-IDF 4.4.3
+
+_1-Wire(R) is a trademark of Maxim Integrated Products, Inc._
---
+++ examples/ds18b20/ds18b20.ino
@@ -0,0 +1,48 @@
+#include "OneWireESP32.h"
+const uint8_t MaxDevs = 2;
+
+float currTemp[MaxDevs];
+
+void tempTask(void *pvParameters){
+	OneWire32 ds(13, 0, 1, 0); //gpio pin, tx, rx, parasite power
+	// There are 8 RMT channels (0-7) available on ESP32 for tx/rx
+	
+	uint64_t addr[MaxDevs];
+	
+	//uint64_t addr[] = {
+	//	0x183c01f09506f428,
+	//	0xf33c01e07683de28,
+	//};
+	
+	//to find addresses
+	uint8_t devices = ds.search(addr, MaxDevs);
+	for (uint8_t i = 0; i < devices; i += 1) {
+		Serial.printf("%d: 0x%llx,\n", i, addr[i]);
+		//char buf[20]; snprintf( buf, 20, "0x%llx,", addr[i] ); Serial.println(buf);
+	}
+	//end
+
+	for(;;){
+		ds.request();
+		vTaskDelay(750 / portTICK_PERIOD_MS);
+		for(byte i = 0; i < MaxDevs; i++){
+			uint8_t err = ds.getTemp(addr[i], currTemp[i]);
+			if(err){
+				const char *errt[] = {"", "CRC", "BAD","DC","DRV"};
+				Serial.print(i); Serial.print(": "); Serial.println(errt[err]);
+			}else{
+				Serial.print(i); Serial.print(": "); Serial.println(currTemp[i]);
+			}
+		}
+		vTaskDelay(3000 / portTICK_PERIOD_MS);
+	}
+} // tempTask
+
+void setup() {
+	delay(1000);
+	Serial.begin(115200);
+	xTaskCreatePinnedToCore(tempTask, "tempTask", 2048,  NULL,  1,  NULL, 0);
+}
+
+
+void loop() {}
